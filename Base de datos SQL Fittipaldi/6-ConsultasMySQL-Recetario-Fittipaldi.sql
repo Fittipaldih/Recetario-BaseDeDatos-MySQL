25c31db69814c23a24b59d46bb20aa52866e9fd3
Hernán Fittipaldi <111435538+Fittipaldih@users.noreply.github.com>
2023-02-25 16:18:13 -0300

    Update 6-ConsultasMySQL-Recetario-Fittipaldi.sql

--- Base de datos SQL Fittipaldi/6-ConsultasMySQL-Recetario-Fittipaldi.sql
+++ Base de datos SQL Fittipaldi/6-ConsultasMySQL-Recetario-Fittipaldi.sql
@@ -4,7 +4,7 @@ USE Recetario_integrador;
 -- i. ¿Cuáles son mis listas de compras activas? 
 select lc.*
 from lista_compras lc where exists (select 1 
-									from lista_compras_incluye_ingrediente lcii 
+				    from lista_compras_incluye_ingrediente lcii 
                                     where lc.codigo = lcii.codigo_lista_compras 
                                     and lcii.comprado_o_no=false);
                                     
@@ -25,7 +25,7 @@ where r.id = (select recetaFavorita.idRecetaFavorita
                                 join menu m on m.id=mcc.id_menu
                                 group by ccr.id_receta, m.fuePreparado
                                 having  m.fuePreparado=true) as cantidadMaxima) 
-				as recetaFavorita);
+			as recetaFavorita);
 	
 -- iv. Reporte de ingredientes por tipo de comida del último mes.
 Select i.nombre, tc.descripcion
@@ -34,10 +34,10 @@ join receta_tiene_ingrediente rti on rti.id_Ingrediente = i.id
 join comida_es_conjunto_receta comrec on comrec.id_receta = rti.id_receta
 join comida c on c.id=comrec.id_comida
 join tipo_comida tc on tc.id = c.id_tipo_comida1
-where exists (select 1
-			from menu_contiene_comida mcc join menu m on m.id=mcc.id_menu
-			where mcc.id_comida = c.id
-			and m.fecha_desde between '2022-11-01' and '2022-12-01')
+where exists (	select 1
+		from menu_contiene_comida mcc join menu m on m.id=mcc.id_menu
+		where mcc.id_comida = c.id
+		and m.fecha_desde between '2022-11-01' and '2022-12-01')
 group by tc.id, tc.descripcion, i.nombre;
 
 
@@ -51,28 +51,32 @@ join receta r on r.id = ccr.id_receta
 join receta_tiene_ingrediente rti on rti.id_receta = r.id
 join ingrediente i on i.id = rti.id_ingrediente
 where exists ( select 1 
-				from menu m join menu_contiene_comida mcc on mcc.id_comida = m.id 
-				where m.id = mcc.id_comida 
-                and month(m.fecha_desde)=month(curdate()) 
+		from menu m 
+	      	join menu_contiene_comida mcc on mcc.id_comida = m.id 
+		where m.id = mcc.id_comida 
+                and month(m.fecha_desde) = month(curdate()) 
                 and year(m.fecha_desde) = year(curdate()))
-	or exists  ( select 1 
-				from menu m join menu_contiene_comida mcc on mcc.id_comida = m.id 
-				where m.id = mcc.id_comida 
-                and month(m.fecha_hasta)=month(curdate()) 
-                and year(m.fecha_hasta) = year(curdate()))
+		or exists  ( select 1 
+			     from menu m 
+			     join menu_contiene_comida mcc on mcc.id_comida = m.id 
+			     where m.id = mcc.id_comida 
+               		     and month(m.fecha_hasta)=month(curdate()) 
+                	     and year(m.fecha_hasta) = year(curdate()))
 group by m.id;
 
 update menu set valor_inflacionario = 5.0 where id = 1;
+
 -- vi. Conocer mis marcas favoritas, es decir, las que más compro.
 select m.descripcion as MarcaFavorita
 from marca m
 where m.id in (select marcaFavorita.idMarcaFavorita
                 from (select max(cantidadMaxima.total), cantidadMaxima.idMarca as idMarcaFavorita
-						from  (select count(lcii.id_Ingrediente) as total, i.id_marca as idMarca
-								from lista_compras_incluye_ingrediente lcii join ingrediente i on i.id=lcii.id_Ingrediente
-								group by i.id_marca, lcii.comprado_o_no
-								having  lcii.comprado_o_no=true) as cantidadMaxima) 	
-						as marcaFavorita);
+			from  (select count(lcii.id_Ingrediente) as total, i.id_marca as idMarca
+				from lista_compras_incluye_ingrediente lcii 
+			       	join ingrediente i on i.id=lcii.id_Ingrediente
+				group by i.id_marca, lcii.comprado_o_no
+				having  lcii.comprado_o_no=true) as cantidadMaxima) 	
+			as marcaFavorita);
 
 -- vii. ¿En qué lapso del año (mes, semana, período de tiempo) preparé todas las recetas de algún influencer?
 select i.nombre as nombreReceta, datediff(max(m.fecha_hasta), min(m.fecha_desde)) as LapsoEnDias
@@ -83,8 +87,9 @@ join comida c on c.id = ccr.id_comida
 join menu_contiene_comida mcc on mcc.id_comida = c.id
 join menu m on m.id = mcc.id_menu
 where not exists (select 1
-					from comida_es_conjunto_receta ccr2 where ccr2.preparada = false
-                    and ccr2.id=ccr.id_receta);
+		  from comida_es_conjunto_receta ccr2 
+		  where ccr2.preparada = false
+                  and ccr2.id=ccr.id_receta);
  
  
 -- viii. ¿Cuáles son las recetas más utilizadas en los menúes del último año?
@@ -95,13 +100,12 @@ join comida c on c.id= ccr.id_comida
 join menu_contiene_comida mcc on mcc.id_comida= c.id
 join menu m on m.id=mcc.id_menu
 and r.id in (select cantidadRecetaFavorita.idReceta
-			from (select max(cantidadMaxima.total),cantidadMaxima.idReceta
-					from (select count(ccr.id_receta) as total, ccr.id_receta as idReceta
-							from comida_es_conjunto_receta ccr
-							where year(m.fecha_desde) = '2022'
-							group by ccr.id_receta) 
-					as cantidadMaxima) 
-				as cantidadRecetaFavorita)
+	     from (select max(cantidadMaxima.total),cantidadMaxima.idReceta
+		   from (select count(ccr.id_receta) as total, ccr.id_receta as idReceta
+			 from comida_es_conjunto_receta ccr
+			 where year(m.fecha_desde) = '2022'
+			 group by ccr.id_receta) as cantidadMaxima) 
+	      as cantidadRecetaFavorita)
 group by r.id, r.comentario;
 
 -- ix. ¿Cuántas comidas por semana preparo en promedio?
@@ -109,7 +113,7 @@ select cantidadComidas.total/cantidad_de_semanas.total as CantidadComidasPromedi
 from (select count(mcc.id_comida) as total
         from menu_contiene_comida mcc) as cantidadComidas,
         (select datediff(curdate(),min(m.fecha_desde))/7 as total
-		from menu m)  as cantidad_de_semanas;
+	from menu m)  as cantidad_de_semanas;
 
 -- x. ¿Cuál es el costo por comensal en promedio, en un mes?
 select c.id as idComensal, avg((i.precio*rti.cantidad)*ccr.cantidad) as costoPorComensal
@@ -120,13 +124,14 @@ join receta r on r.id = ccr.id_receta
 join receta_tiene_ingrediente rti on rti.id_receta = r.id
 join ingrediente i on i.id = rti.id_ingrediente
 where exists ( select 1 
-				from menu m join menu_contiene_comida mcc on mcc.id_menu = m.id 
-				where m.fecha_desde between date_sub(curdate(), interval 30 day) -- date sub devuelve la diferencia de tipos date. Curdate es la fecha actual
+		from menu m 
+	     	join menu_contiene_comida mcc on mcc.id_menu = m.id 
+		where m.fecha_desde between date_sub(curdate(), interval 30 day) -- date sub devuelve la diferencia de tipos date. Curdate es la fecha actual
                 and curdate() 
                 and com.id = mcc.id_comida )
-	or exists  ( select 1 
-				from menu m join menu_contiene_comida mcc on mcc.id_menu = m.id 
-				where m.fecha_hasta between date_sub(curdate(), interval 30 day) 
-                and curdate() 
-                and com.id = mcc.id_comida )
+		or exists  ( 	select 1 
+				from menu m 
+			    	join menu_contiene_comida mcc on mcc.id_menu = m.id 
+				where m.fecha_hasta between date_sub(curdate(), interval 30 day) and curdate() 
+                		and com.id = mcc.id_comida )
 group by c.id;
